bb3b6ed7921044ae9d79bc48115a512a82d2f8dc
FernandaCidade <email@email.com>
2025-01-03 14:10:39 -0300

    :tada: Primero commit

---
+++ random_1.py
@@ -0,0 +1,29 @@
+import random
+
+def main():
+    # Fornece os limites de um intervalo de números e deixa o usuário adivinhar o número do computador até a suposição estiver correta.
+
+    menor = int(input("Entre com o menor número: "))
+    maior = int(input("Entre com o maior número: "))
+
+    meuNumero = random.randint(menor,maior)
+
+    count = 0
+
+    while True:
+        count += 1 
+        numUsuario = int(input("Entre com seu chute: "))
+
+        if numUsuario < meuNumero: 
+            print("Muito pouco")
+        
+        elif numUsuario > meuNumero:
+            print ("Muito longe")
+        
+        else: 
+            print("Você conseguiu em ", count, " Tentativas! Apenas!")
+            break
+
+
+    if __name__ == "__main__": 
+        main()
